f85605a5ae6aab36db33c0ccb6958858758a4689
Alexey Marinkevich <alexeymarinkevich@gmail.com>
2023-07-24 19:31:31 +0900

    initial grid markup

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,15 +1,24 @@
-const Grid = () => (
-  <div>
-    <div id="1">1</div>
-    <div id="2">2</div>
-    <div id="3">3</div>
-    <div id="4"></div>
-    <div id="5"></div>
-    <div id="6"></div>
-    <div id="7"></div>
-    <div id="8"></div>
-    <div id="9"></div>
-  </div>
-);
+const Grid = () => {
+  return (
+    <div
+      className="grid grid-cols-3 text-center
+    [&>*:nth-child(-n+6)]:border-b-2 [&>*]:font-bold 
+    [&>*]:border-r-2 [&>*:nth-child(3n)]:border-r-0
+    [&>*]:p-6
+    "
+      onClick={({ target }) => onClickAction(target.id)}
+    >
+      <div id="1"></div>
+      <div id="2"></div>
+      <div id="3"></div>
+      <div id="4"></div>
+      <div id="5"></div>
+      <div id="6"></div>
+      <div id="7"></div>
+      <div id="8"></div>
+      <div id="9"></div>
+    </div>
+  );
+};
 
 export default Grid;
